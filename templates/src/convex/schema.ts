e51fece1e13979a22a66a175fb247e6a3ae1651e
Nathan Hoyt <slimnatekc@gmail.com>
2025-11-06 03:52:22 -0600

    Update schema.ts template to not include extra fields or tables by default

--- templates/src/convex/schema.ts
+++ templates/src/convex/schema.ts
@@ -3,20 +3,7 @@ import { v } from "convex/values";
 import { extendUsers } from "workos-convex-sveltekit/schema";
 
 export default defineSchema({
-  users: extendUsers({
-    extraField: v.optional(v.string()),
-  })
-  .index("by_extra_field", ["extraField"]),
-
-  public_data: defineTable({
-    text: v.string(),
-    userId: v.optional(v.string()),
-  }),
-
-  private_data: defineTable({
-    text: v.string(),
-    userId: v.optional(v.string()),
-  }),
+  users: extendUsers({})
 });
 
 
